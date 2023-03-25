9ba28ae4a21b8c49dc8d5c55ebb31a6bff35a681
lepeshkinff <lepeshkinff@gmail.com>
2023-03-25 07:45:34 +0300

    form

---
+++ Amati.UI/Forms/ConcretetableForm.cs
@@ -0,0 +1,12 @@
+﻿using Amati.Engine.Services;
+
+namespace Amati.UI.Forms
+{
+    public class ConcretetableForm<T> : TableViewForm
+    {
+        public ConcretetableForm(IDataSource<T>? palinsDataSource) 
+            : base(palinsDataSource)
+        {
+        }
+    }
+}
